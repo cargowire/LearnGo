3cbedcc76447098a2ad095e0076ef31898c60a30
Craig Rowe <craig@cargowire.net>
2022-04-03 22:50:28 +0100

    Select single row

--- data-access/main.go
+++ data-access/main.go
@@ -41,6 +41,12 @@ func main() {
     }
     fmt.Printf("Albums found: %v\n", albums)
 
+    alb, err := albumByID(2)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("Album found: %v\n", alb)
+
     pingErr := db.Ping()
     if pingErr != nil {
         log.Fatal(pingErr)
@@ -70,4 +76,19 @@ func albumsByArtist(name string) ([]Album, error) {
         return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
     }
     return albums, nil
+}
+
+// albumByID queries for the album with the specified ID.
+func albumByID(id int64) (Album, error) {
+    // An album to hold data from the returned row.
+    var alb Album
+
+    row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+    if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+        if err == sql.ErrNoRows {
+            return alb, fmt.Errorf("albumsById %d: no such album", id)
+        }
+        return alb, fmt.Errorf("albumsById %d: %v", id, err)
+    }
+    return alb, nil
 }
